d5bb1942c0bd2d907d722a296e9ec516ad314761
praneetn <32501081+praneetn@users.noreply.github.com>
2023-01-14 20:58:07 -0500

    Changed commit-stage file

--- src/test/java/com/we/us/catalogservice/service/BookServiceTest.java
+++ src/test/java/com/we/us/catalogservice/service/BookServiceTest.java
@@ -9,8 +9,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
